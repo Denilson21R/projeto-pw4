ae940dd1bcec008d3be8ecd63bc4b9912938988d
Denilson21R <45276744+Denilson21R@users.noreply.github.com>
2023-03-24 00:14:00 -0300

    initial use of sequelize with mysql

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const {Sequelize} = require("sequelize");
 const app = express();
 
 const port = 8080;
+
+const sequelize = new Sequelize('pw4-projeto', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+//works correctly
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
 
 //importing routes
 app.use("/user", require("./routes/user"));
--- backend/model/user.js
+++ backend/model/user.js
@@ -1,3 +1,26 @@
-class User{
+import {DataTypes} from "sequelize";
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
-}
+//TODO: use a config file to save db config
+const sequelize = new Sequelize('pw4-projeto', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+class User extends Model {}
+
+//TODO: implement correctly
+User.init({
+    // Model attributes are defined here
+    id: {
+        type: DataTypes.INTEGER
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: 'User' // We need to choose the model name
+});
